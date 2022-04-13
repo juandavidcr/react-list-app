9c90a22130e8bba94d75813f8ae3a9ca58bc6316
juan_david_cr <j1dcrane@gmail.com>
2022-04-13 15:48:31 -0500

    using cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import ExpenseItem from "./components/ExpenseItem";
-
+import Expenses from "./components/Expenses";
 function App() {
-  
   const expenses = [
     {id:'0001',title:"Seguro de AUTO",amount:299.99,date:new Date(2022,3,13)},
-    {id:'00012',title:"Seguro de VIDA",amount:199.99,date:new Date(2022,3,13)},
-    {id:'00013',title:"Seguro de GMM",amount:499.99,date:new Date(2022,3,13)},
+    {id:'0002',title:"Seguro de VIDA",amount:199.99,date:new Date(2022,3,13)},
+    {id:'0003',title:"Seguro de GMM",amount:499.99,date:new Date(2022,3,13)},
   ];
+  
   return (
      <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem  title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}/>
-      <ExpenseItem  title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}/>
-      <ExpenseItem  title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}/>
-      
-    
+      <Expenses items={expenses}/>
     </div>
   );
 }
