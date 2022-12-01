620eaa6d158e0b2a9006f37565eefcf882aa9035
juan_david_cr <j1dcrane@gmail.com>
2022-11-30 23:59:25 -0600

    Ya crea nuevos items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
+import React,{useState} from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-const App=() =>{
-  const expenses = [
+const DUMMY_EXPENSES = [
     {id:'0001',title:"Seguro de AUTO",amount:299.99,date:new Date(2022,3,13)},
     {id:'0002',title:"Seguro de VIDA",amount:199.99,date:new Date(2022,3,13)},
     {id:'0003',title:"Seguro de GMM",amount:499.99,date:new Date(2022,3,13)},
   ];
+  const App=() =>{
+  const [expenses,setExpenses] =useState(DUMMY_EXPENSES);
   
   const addExpenseHandler = (expense)=>{
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses(prevExpenses =>{
+      return [expense,...prevExpenses];
+    });
   };
   return (
      <div>
